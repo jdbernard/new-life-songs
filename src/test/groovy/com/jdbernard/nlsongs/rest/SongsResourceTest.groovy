d84ec06afb7bfa139ead5c8ad1a7705cfdc6db12
Jonathan Bernard <jdb@jdb-labs.com>
2015-03-03 16:27:45 -0600

    Started working on an embeddable test version of the service for unit testing.

---
+++ src/test/groovy/com/jdbernard/nlsongs/rest/SongsResourceTest.groovy
@@ -0,0 +1,12 @@
+package com.jdbernard.nlsongs.rest
+
+import com.jdbernard.net.HttpContext
+import org.junit.Test
+import org.junit.AfterClass
+import org.junit.BeforeClass
+
+import static org.junit.Assert.*
+
+public class SongsResourceTest {
+
+}
---
+++ src/test/groovy/com/jdbernard/nlsongs/service/NLSongsDBTest.groovy
@@ -0,0 +1,11 @@
+package com.jdbernard.nlsongs.service
+
+import org.junit.Test
+import org.junit.AfterClass
+import org.junit.BeforeClass
+
+import static org.junit.Assert.*
+
+public class NLSongsDBTest {
+
+}
