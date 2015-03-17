f6f3222855de2706bd3008bffb1d14691ff40086
Jonathan Bernard <jdb@jdb-labs.com>
2015-03-17 14:40:54 -0500

    Started working on front-end MVC (WIP).

--- src/main/webapp/js/new-life-songs.js
+++ src/main/webapp/js/new-life-songs.js
@@ -2,7 +2,31 @@
 
 	var NLS = window.NewLifeSongs = {};
 
+    //  #######################################################################
 	/// ## Models
+    //  #######################################################################
 
+    /// ### SongModel
+    NLS.SongModel = Backbone.Model.extend({ });
+
+    /// ### ServiceModel
+    NLS.ServiceModel = Backbone.Model.extend({ });
+
+    /// ### PerformanceModel
+    NLS.PerformanceModel = Backbone.Model.extend({ });
+
+    //  #######################################################################
 	/// ## Views
+    //  #######################################################################
+
+    /// ### SongsView
+    NLS.SongsView = Backbone.View.extend({
+
+    });
+
+    /// ### ServicesView
+    NLS.ServicesView = Backbone.View.extend({
+
+    });
+
 })();
